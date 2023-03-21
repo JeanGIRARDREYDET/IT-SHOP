669505a3952973fedf121efd2c3a9575f818f397
JeanGIRARDREYDET <cogitafac@gmail.com>
2023-03-21 17:18:05 +0100

    Ajour de la version Grid

--- front/src/components/productCard/ProductCard.tsx
+++ front/src/components/productCard/ProductCard.tsx
@@ -2,10 +2,9 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Style from "./Productcard.module.css";
-
-
-
 import { Link } from 'react-router-dom';
+
+
 export interface IProduct {
   _id?: object | undefined;
   name: string;
@@ -24,21 +23,27 @@ type Props = {
 }
 
 const ProductCard = ({product}: Props) => {
-  return (
-    <div className="card">
-      <div className="header">
-        <img/>
-      </div>
-      <div className="main">
+  return ( 
+    <Card className={Style.Card} sx={{ width: 250 }}>
+      <CardContent>
+
+        <div className="header">
+          <img></img>
+        </div>
+        <div className="main">
         
         <Link to={`/product/${product._id}`} >{product.name}</Link>
         
         name : {product.name}
-        
-      </div>
-      <div className="footer">
-   </div>
-</div>
+        </div>
+        <div className="footer">
+
+        </div>
+      </CardContent>
+
+    </Card>  
+
+
   )
 }
 
