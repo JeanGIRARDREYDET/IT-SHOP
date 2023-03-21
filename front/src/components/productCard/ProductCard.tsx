4bf88b4cef6b245149131124084450278487f6ff
ManuelSince <ManuelSince@users.noreply.github.com>
2023-03-21 16:53:34 +0100

    fix

--- front/src/components/productCard/ProductCard.tsx
+++ front/src/components/productCard/ProductCard.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -6,9 +5,6 @@ import Style from "./Productcard.module.css";
 
 
 
-=======
-import { Link } from 'react-router-dom';
->>>>>>> 63b3af4c06a7125bd7fc33a8a10b192bf4c9ad7c
 export interface IProduct {
   _id?: object | undefined;
   name: string;
@@ -27,7 +23,6 @@ type Props = {
 }
 
 const ProductCard = ({product}: Props) => {
-<<<<<<< HEAD
   return ( 
     <Card className={Style.Card} sx={{ width: 250 }}>
       <CardContent>
@@ -45,21 +40,6 @@ const ProductCard = ({product}: Props) => {
 
     </Card>  
 
-=======
-  return (
-    <div className="card">
-      <div className="header">
-        <img></img>
-      </div>
-      <div className="main">
-        <>
-        <Link to={`/products/${product._id}`} >{product.name}</Link>
-        </>
-        <>name : {product.name}</>
-        
-      </div>
-      <div className="footer">
->>>>>>> 63b3af4c06a7125bd7fc33a8a10b192bf4c9ad7c
 
   )
 }
