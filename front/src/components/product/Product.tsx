377607ae3a28947f03c6872b0c621e001fbea14d
JeanGIRARDREYDET <cogitafac@gmail.com>
2023-03-22 11:05:42 +0100

    Ajour des fiches produit

--- front/src/components/product/Product.tsx
+++ front/src/components/product/Product.tsx
@@ -6,12 +6,32 @@ const Product = () => {
     let { id } = useParams();
     const location = useLocation();
     const product = location.state?.data;
+    console.log(product);
     return (
     <>
     <h1>The Best Product is {id}</h1>
     <div>name : {product.name}</div>
     <div>brand : {product.brand}</div>
     <div>description : {product.description}</div>
+
+
+
+    <div>categories : {product.categories.map((item:string) => 
+                             <div>cat-{item}-</div>
+                           )}</div>
+    <div>images : {product.images}</div>
+
+   
+   
+    { product.images.map((item:string) => 
+                             <div>img-{item}-</div>
+                           )
+                           }
+
+  <div>Prix : {product.price}</div>
+  <div>stock : {product.stock}</div>
+  <div>rating : {product.rating}</div>
+  <div>description : {product.createdAt}</div>
 
     </>
     )
