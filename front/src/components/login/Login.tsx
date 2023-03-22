aebf6cf0d50d0903d3d164740a167399dd336e9f
ManuelSince <ManuelSince@users.noreply.github.com>
2023-03-22 08:52:52 +0100

    business logic Login component

--- front/src/components/login/Login.tsx
+++ front/src/components/login/Login.tsx
@@ -1,7 +1,15 @@
 import { Box, Button, FormControl, FormHelperText, Input, InputLabel } from '@mui/material'
+import { useState } from 'react'
 import Styles from './Login.module.css'
 
 const Login = ()=> {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleLoginForm = () => {
+    if (email !== "" && password !== "") {
+      console.log(email, password)
+    }  
+  }
  return (
   <>
   <h1>Login</h1>
@@ -10,16 +18,16 @@ const Login = ()=> {
       
     
       <InputLabel htmlFor="email-login">Email address</InputLabel>
-      <Input id="email-login" aria-describedby="my-helper-text" type="text" />
-      <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+      <Input id="email-login" aria-describedby="my-helper-text" type="text" onKeyUp={(event)=>setEmail(event.currentTarget.value)}/>
+
     </Box>
     <Box sx={{p: 2}}>
       <InputLabel htmlFor="password-login">Password</InputLabel>
-      <Input id="password-login" aria-describedby="my-helper-text" type="password" />
-      <FormHelperText id="my-helper-text">We'll never share your password.</FormHelperText>
+      <Input id="password-login" aria-describedby="my-helper-text" type="password" onKeyUp={(event)=>setPassword(event.currentTarget.value)}/>
+
     </Box>
     <Box sx={{p: 2}}>
-      <Button variant="outlined">Se connecter</Button>
+      <Button variant="outlined" onClick={handleLoginForm} >Se connecter</Button>
     </Box>
       
   </>
