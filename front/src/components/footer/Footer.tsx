c621f91b54cb8a96ff3e6e036340827b3cf57d22
demba75009 <b-demba@hotmail.fr>
2023-03-23 16:04:53 +0100

    footer

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -1,4 +1,9 @@
 import './Footer.css'
+
+import LocalPhoneTwoToneIcon from '@mui/icons-material/LocalPhoneTwoTone';
+import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
+import MailOutlineTwoToneIcon from '@mui/icons-material/MailOutlineTwoTone';
+
 const Footer = () => {
   return (
     <div id='footer'>
@@ -10,9 +15,9 @@ const Footer = () => {
       <div id="footershits" className="">
         <ul>
           { /*  or <li> via children ?  ADD ROUTER */ }
-          <li className="">footer item 1</li>
-          <li className="">footer item 2</li>
-          <li className="">footer item 3</li>
+          <li className=""><LocalPhoneTwoToneIcon />: +0145789956</li>
+          <li className=""><MailOutlineTwoToneIcon />: It-Shop@gmail.fr</li>
+          <li className=""><FacebookTwoToneIcon/>: II-SHOP</li>
         </ul>
       </div>
     </div>
