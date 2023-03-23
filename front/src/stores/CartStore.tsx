8e24e9dfa2676edc8738a08af4a7fa46316541b8
ManuelSince <ManuelSince@users.noreply.github.com>
2023-03-23 08:36:37 +0100

    add 2 stores : a session store and a cart store, need a useContext now

--- front/src/stores/CartStore.tsx
+++ front/src/stores/CartStore.tsx
@@ -1,6 +1,32 @@
-const CartStore = () => {
-  return (
-    <></>
-  )
+import { IProduct } from '../types/product'
+
+type IAction = {
+  type: string
+  payload: IProduct | string,
+
 }
-export default CartStore
+
+type IState = {
+  cart: IProduct[]
+}
+
+export const initialCartState = {
+  cart: []
+};
+
+export const cartReducer = (state: IState, action: IAction) => {
+ switch (action.type) {
+     case 'addProduct':
+     return {
+         ...state,
+         cart: [...state.cart, action.payload]
+     };
+     case 'removeProduct':
+      return {
+        ...state,
+        cart: [...state.cart.filter(product => product._id !== action.payload)]
+      }
+     default:
+     return state;
+ }
+};
--- front/src/stores/SessionStore.tsx
+++ front/src/stores/SessionStore.tsx
@@ -1,7 +1,28 @@
-const SessionStore = () => {
 
-  return (
-    <></>
-  )
+type IAction = {
+  type: string
+  payload: string,
+
 }
-export default SessionStore
+
+export const initialSessionState = {
+  userID: '',
+  role: ''
+};
+
+export const sessionReducer = (state: any, action: IAction) => {
+ switch (action.type) {
+     case 'setUserID':
+     return {
+         ...state,
+         userID: action.payload
+     };
+     case 'setRole':
+      return {
+        ...state,
+        role: action.payload
+      }
+     default:
+     return state;
+ }
+};
