d58cba522b16c5bbf3c216aece4e66b5abe59977
ManuelSince <ManuelSince@users.noreply.github.com>
2023-03-24 20:37:29 +0100

    making ProductsFiltering system work a bit

--- front/src/components/products/Products.tsx
+++ front/src/components/products/Products.tsx
@@ -7,29 +7,65 @@ import useFetch from "../../hooks/useFetch";
 import { IProduct } from "../../types/product";
 
 
+
 const Products = () => {
-  const [fetchRes, setFetchRes] = useState([])
-  const [filter, setFilter] = useState({})
+  const [prods, setProds] = useState([])
+  const [filter, setFilter] = useState({name: '', brand: '', category: '', rating: -1, priceRange: []})
 
   const {data, err} = useFetch<IProduct[] | []>('/products')
 
   const handleFiltering = (filter: any) => {
     setFilter(f => ({...f, ...filter}))
   }
+
+  const filtering = (p: IProduct) => {
+    // const filtres = Object.keys(filter).filter(key => filter[key] !== "" || filter[key] !== [])
+     const completeFilterExample= {
+        brand: "dzzdzd",
+        category: "automobile",
+        name: "zddz",
+        priceRange: [ 3841, 6113 ],
+        rating: 4
+      }
+      // const filteringreturnexample = el.Age >=15 && el.RollNumber <= 200 && el.Marks >= 80 ;
+    console.log(filter.name)
+    return filter.name !== "" ? new RegExp(filter.name, 'i').test(p.name): true
+        //&& filter.brand !== ""? new RegExp(filter.brand, 'i').test(p.brand): false
+        //&& filter.category !== "" ? p.categories.includes(filter.category): false
+  }
+  const regName = (val: string) => new RegExp(filter.name, 'gi').test(val)
+  const regBrand = (val: string) => new RegExp(filter.brand, 'gi').test(val)
+
+  const filterByName = (p: IProduct) => {
+    if(filter.hasOwnProperty('name') && filter.name !== "") {
+      regName(p.name) ? true: false
+    } else {
+      return true
+    }
+  }
+  const globalFilter = (ps: IProduct[]):IProduct[] | [] => {
+    const fn = filter.name !== '' ? ps.filter(p => regName(p.name)) : ps
+    const fb = filter.brand !== '' ? fn.filter(p => regBrand(p.brand)) : fn
+    const fr = filter.rating !== -1 ? fb.filter(p => p.rating >= filter.rating) : fb
+    const fc = filter.category !== '' ? fr.filter(p => p.categories.includes(filter.category)) : fr
+    const fp = filter.priceRange.length === 2 ? fc.filter(p => p.price > filter.priceRange[0] && p.price < filter.priceRange[1]) : fc
+    return fp
+  }
+
   useEffect(() => {
     if(err)
       console.log(err)
     if(data)
-      setFetchRes(prods => [...prods, ...data])
+      setProds(p => [...p, ...data])
   }, [data]) 
   
   return (
     <><h1>Products</h1>
 
       <Box sx={{}}>
-      <ProductFilter  products={fetchRes} onFilter={handleFiltering} />
+      <ProductFilter  products={prods} onFilter={handleFiltering} />
       <div className={Style.ListProducts}>
-        { fetchRes.map((product, index) => {
+        { globalFilter(prods).map((product, index) => {
           //make the filtering logic here with filter
           return (
           
