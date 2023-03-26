7d033dc9a6bc1152cbf355c5560ff4f5d3a59c03
ManuelSince <ManuelSince@users.noreply.github.com>
2023-03-26 17:21:56 +0200

    instructions dans la page checkout

---
+++ front/src/pages/CheckOutPage.tsx
@@ -0,0 +1,13 @@
+const CheckOutPage = () => {
+  return (
+    <>
+      {/* choix du moyen, de paiement => select ou autre direct sur la page sur la page*/ } 
+      {/*enregistrement des données bancaires  => composant dédié */ }
+      {/* choix des adresses de livraison et ou facturation composant dédié p e*/ }
+      {/* choix d'un depot à proximité ? */ }
+      {/*recapitulatif panier taxes etc => sur la page */ }
+      {/**/ }
+    </>
+  )
+}
+export default CheckOutPage
