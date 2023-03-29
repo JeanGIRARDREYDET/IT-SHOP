102bfc19a75092e6cfabc3be20662be288ed5a0d
ManuelSince <ManuelSince@users.noreply.github.com>
2023-03-29 08:58:15 +0200

    add paiement checkout, must add it to user in the store

--- front/src/pages/CheckOutPage.tsx
+++ front/src/pages/CheckOutPage.tsx
@@ -1,10 +1,24 @@
-import { Box } from "@mui/material"
+import { Box, Button, ButtonGroup } from "@mui/material"
+import { useState } from "react";
+import CbForm from '../components/cbForm/CbForm'
+import { CartConsumerHook } from "../context/CartContext";
+import { IPaiement } from "../types/user";
 
 const CheckOutPage = () => {
+  const [{user, cart}, dispatch] = CartConsumerHook();
+  const [paiementMethod, setPaiementMethod] = useState('')
+  const handlePaiement = (value: IPaiement) => {
+    console.log(value)
+  }
   return (
     <>
       <Box sx={{p: 2, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}} >
       <h1>Paiement</h1>
+      <ButtonGroup>
+        <Button onClick={() => setPaiementMethod('cb')}>CB</Button>
+        <Button onClick={() => setPaiementMethod('paypal')}>Paypal</Button>
+      </ButtonGroup>
+      {paiementMethod === 'cb' && <CbForm onCBFill={handlePaiement} />}
       {/* choix du moyen, de paiement => select ou autre direct sur la page sur la page*/ } 
       {/*enregistrement des données bancaires  => composant dédié */ }
       {/* choix des adresses de livraison et ou facturation composant dédié p e*/ }
