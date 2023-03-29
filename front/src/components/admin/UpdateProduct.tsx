294733a8ca943df67ff56a7910206d2ef4db42ca
ManuelSince <ManuelSince@users.noreply.github.com>
2023-03-29 17:05:05 +0200

    productForm

--- front/src/components/admin/UpdateProduct.tsx
+++ front/src/components/admin/UpdateProduct.tsx
@@ -1,16 +1,15 @@
 import { Box, Button, Input, InputLabel } from "@mui/material"
-import { SyntheticEvent, useState } from "react"
+import { SyntheticEvent, useEffect, useState } from "react"
 import { IProduct } from "../../types/product"
 import ProductForm from "../productForm/ProductForm"
 import SearchBar from "../searchBar/SearchBar"
 
 const UpdateProduct = () => {
-  const [product, setProduct] = useState<IProduct | null>(null)
+  const [prod, setProduct] = useState<IProduct | null>(null)
   const [isProductForm, setisProductForm] = useState(false)
 
   const handleSearch = (event: SyntheticEvent<Element, Event>, value: IProduct | null) => {
-    console.log(`${value}`)
-    setProduct(prev => ({...prev, ...value}))
+    setProduct(prev => ({...prev,...value}))
     setisProductForm(true)
     // return redirect(`/product/${value?._id}`)
   }
@@ -19,25 +18,16 @@ const UpdateProduct = () => {
 
   }
 
-  const prod = {
-    _id: {"$oid": "641208cfc6d56d7d313ba5be"},
-    name: "Multiple-Chargeur.",
-    brand: "LENCENT",
-    description:"\n[Prise USB 6 en 1] 2 AC sortie, 3 ports USB et 1 port USB C sont la combinaison parfaite. Vous pouvez utiliser ce prise pour charger jusqu’à 6 appareils en même temps, la recharge ne nécessite pas d’attente. Installez facilement la prise de USB sur le mur. Ne vous inquiétez pas lorsque vous chargez vos appareils et votre appareil mobile.\n[Utilisation universelle]Le chargeur rapide USB C de 15W est équipé d’un port d’alimentation USB-C et d’une puissance de sortie de 15W pour recharger iPhone, iPad, le téléphone et la tablette à pleine vitesse. AC prise: Puissance maximale jusqu'à 4000 watts (max. 250 V, 16 A). Tension d'entrée des ports USB: 100 V - 240 V 0.3A. Sortie USB: 5V/3.4A(total), 5V/2.4A(connexion unique) Sortie USB C: 5V/3A 15W.\n[Technologie auto-ID exclusive]LENCENT la technologie AUTO-ID UNIQUE identifie automatiquement tous vos appareils et fournit le meilleur courant de charge pour une charge maximale et sûre.\n[Sécurité Supérieure]Boîtier de Matériau ignifuge en PC 750 °C. Les stores de sécurités internes empêchent les articles d’être insérés dans des prises pour assurer la sécurité de vous et de votre enfant. Les Protection de sécurité sont efficacement améliorées pour prévenir la surpression, la surcharge, le surcourage, le court-circuit, la surchauffe, la surcharge, etc.\n[Économisez de l’espace]Gagnez du temps et des efforts, sans avoir besoin de fils supplémentaires. Il y a un support téléphonique sur le dessus de le produit qui peut être placé sur là. Elle s’adapte aux prises de courant et ne tombe pas facilement du mur.La taille de 91.3 x 66.2 x 41.1 mm est pratique et compacte pour les voyages d’affaires ou à la maison.",
-    images: ["LENCENT-Multiple-Chargeur1.jpg","LENCENT-Multiple-Chargeur2.jpg","LENCENT-Multiple-Chargeur3.jpg","LENCENT-Multiple-Chargeur4.jpg","LENCENT-Multiple-Chargeur5.jpg"],
-    categories:["Chargeurs secteur"],
-    price:{"$numberDouble":"46.3"},
-    stock:{"$numberInt":"16"},
-    createdAt:{"$date":{"$numberLong":"1642963210000"}},
-    rating:{"$numberInt":"4"}
-  }
+  useEffect(() => {
+
+  }, [prod])
   return (
     <>
       <Box sx={{p: 2, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}} >
-        {!product && <h2>{`Modifier un produit`}</h2>}
-        {product &&<h2>{`Modifier ${product?.brand} de la marque ${product?.brand}`}</h2>}
+        {!prod && <h2>{`Modifier un produit`}</h2>}
+        {prod &&<h2>{`Modifier ${prod?.brand} de la marque ${prod?.brand}`}</h2>}
         <SearchBar onSearch={handleSearch} />
-        {isProductForm && <ProductForm product={product} />}
+        {prod && <ProductForm product={prod} />}
       </Box>
       
     </>
--- front/src/components/productForm/ProductForm.tsx
+++ front/src/components/productForm/ProductForm.tsx
@@ -7,13 +7,12 @@ import Style from "./productForm.module.css"
 type Props = {
   product: IProduct | null
 }
-const ProductForm = ( { product }: Props) => {
 
-  
+const ProductForm = ( { product }: Props) => {
 
   const navigate = useNavigate();
 
-  const [name, setName] = useState( product!==null? product.name :'')
+  const [name, setName] = useState( product !==null? product.name :'')
   const [brand, setBrand] = useState(product!==null? product.brand :'')
   const [description, setDescription] = useState(product!==null? product.description :'')
   const [images, setImages] = useState<string>('')
@@ -23,45 +22,29 @@ const ProductForm = ( { product }: Props) => {
   const [rating, setRating] = useState<number | null>(product!==null? product.rating :0)
   
   const updateProduct = () => {
-    //fetch products/update
-
-
-
 
     const prod = {name, brand, description, images, categories, price, stock, rating,createdAt:new Date().toDateString()} 
-     
-    
-    console.log(name)
-      const transformeStringToArray = images.split(",")
- 
-
+    const transformeStringToArray = images.split(",")
     prod.images = transformeStringToArray
-    
-
     const requestOptions = { method: 'PUT', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({product: prod})};
     fetch(`${import.meta.env.VITE_API_URL}products/update`, requestOptions)
     .then(res=>{
         if(res.ok){
           navigate('/')
-          
           alert("Produit bien modifiez !")
         }
     })
-    
   } 
 
-  const CreateProduct = () => {
+  const createProduct = () => {
     // fetch /products/add
     const prod = {name, brand, description, images, categories, price, stock, rating,createdAt:new Date().toDateString()} 
   
     const transformeStringToArray = images.split(",")
- console.log(transformeStringToArray)
+    console.log(transformeStringToArray)
 
     prod.images = transformeStringToArray
-    
     prod.rating = 1
-  
-    
 
     const requestOptions = { method: 'POST', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({product: prod})};
     fetch(`${import.meta.env.VITE_API_URL}/products/add`, requestOptions)
@@ -74,22 +57,12 @@ const ProductForm = ( { product }: Props) => {
     })
     
   }
+  const watchProd = () => {
 
-  const prodExample = {
-    _id: {"$oid": "641208cfc6d56d7d313ba5be"},
-    name: "Multiple-Chargeur.",
-    brand: "LENCENT",
-    description:"\n[Prise USB 6 en 1] 2 AC sortie, 3 ports USB et 1 port USB C sont la combinaison parfaite. Vous pouvez utiliser ce prise pour charger jusqu’à 6 appareils en même temps, la recharge ne nécessite pas d’attente. Installez facilement la prise de USB sur le mur. Ne vous inquiétez pas lorsque vous chargez vos appareils et votre appareil mobile.\n[Utilisation universelle]Le chargeur rapide USB C de 15W est équipé d’un port d’alimentation USB-C et d’une puissance de sortie de 15W pour recharger iPhone, iPad, le téléphone et la tablette à pleine vitesse. AC prise: Puissance maximale jusqu'à 4000 watts (max. 250 V, 16 A). Tension d'entrée des ports USB: 100 V - 240 V 0.3A. Sortie USB: 5V/3.4A(total), 5V/2.4A(connexion unique) Sortie USB C: 5V/3A 15W.\n[Technologie auto-ID exclusive]LENCENT la technologie AUTO-ID UNIQUE identifie automatiquement tous vos appareils et fournit le meilleur courant de charge pour une charge maximale et sûre.\n[Sécurité Supérieure]Boîtier de Matériau ignifuge en PC 750 °C. Les stores de sécurités internes empêchent les articles d’être insérés dans des prises pour assurer la sécurité de vous et de votre enfant. Les Protection de sécurité sont efficacement améliorées pour prévenir la surpression, la surcharge, le surcourage, le court-circuit, la surchauffe, la surcharge, etc.\n[Économisez de l’espace]Gagnez du temps et des efforts, sans avoir besoin de fils supplémentaires. Il y a un support téléphonique sur le dessus de le produit qui peut être placé sur là. Elle s’adapte aux prises de courant et ne tombe pas facilement du mur.La taille de 91.3 x 66.2 x 41.1 mm est pratique et compacte pour les voyages d’affaires ou à la maison.",
-    images: ["LENCENT-Multiple-Chargeur1.jpg","LENCENT-Multiple-Chargeur2.jpg","LENCENT-Multiple-Chargeur3.jpg","LENCENT-Multiple-Chargeur4.jpg","LENCENT-Multiple-Chargeur5.jpg"],
-    categories:["Chargeurs secteur"],
-    price:{"$numberDouble":"46.3"},
-    stock:{"$numberInt":"16"},
-    createdAt:{"$date":{"$numberLong":"1642963210000"}},
-    rating:{"$numberInt":"4"}
   }
  useEffect(()=>{
 
-
+  console.log(product)
 
  },[product])
   
@@ -97,19 +70,15 @@ const ProductForm = ( { product }: Props) => {
       <div className={Style.productForm}>
       <Box sx={{p: 2}}>
         <InputLabel htmlFor="name-product">Nom</InputLabel>
-        <Input id="name-product" aria-describedby="my-helper-text" type="text" defaultValue={product?.name} onChange={(event)=>{setName(event.currentTarget.value)
-
-        event.target.value = event.currentTarget.value
-          
-        } }     />
+        <Input id="name-product" aria-describedby="my-helper-text" type="text" defaultValue={product?.name} onChange={(event)=>{setName(event.currentTarget.value)}} />
       </Box>
       <Box sx={{p: 2}}>
         <InputLabel htmlFor="brand-product">Marque</InputLabel>
-        <Input id="brand-product" aria-describedby="my-helper-text" type="text" defaultValue ={product?.brand} onKeyUp={(event)=>setBrand(event.currentTarget.value)} />
+        <Input id="brand-product" aria-describedby="my-helper-text" type="text" defaultValue={product?.brand} onKeyUp={(event)=>setBrand(event.currentTarget.value)} />
       </Box>
       <Box sx={{p: 2}}>
         <InputLabel htmlFor="description-product">Description</InputLabel>
-        <Input id="description-product" aria-describedby="my-helper-text" type="text" onKeyUp={(event)=>setDescription(event.currentTarget.value)} defaultValue ={product?.description} />
+        <Input id="description-product" aria-describedby="my-helper-text" type="text" onKeyUp={(event)=>setDescription(event.currentTarget.value)} defaultValue={product?.description} />
       </Box>
       <Box sx={{p: 2}}>
         <InputLabel htmlFor="images-product">Images</InputLabel>
@@ -122,16 +91,14 @@ const ProductForm = ( { product }: Props) => {
       </Box>
       <Box sx={{p: 2}}>
         <InputLabel htmlFor="price-product">Prix</InputLabel>
-        <Input id="price-product" aria-describedby="my-helper-text" type="number" defaultValue ={product?.price} onKeyUp={(event)=>setPrice(event.currentTarget.value)} />
+        <Input id="price-product" aria-describedby="my-helper-text" type="number" defaultValue={product?.price} onKeyUp={(event)=>setPrice(event.currentTarget.value)} />
       </Box>
       <Box sx={{p: 2}}>
         <InputLabel htmlFor="stock-product">Stock</InputLabel>
         <Input id="stock-product" defaultValue ={product?.stock} aria-describedby="my-helper-text" type="number" onKeyUp={(event)=>setStock(event.currentTarget.value)} />
       </Box>
-
-
       <Box sx={{p: 2}}>
-            {product !== null ? (<Button variant="outlined" onClick={updateProduct} >Modifier ce produit</Button>): (<Button variant="outlined" onClick={(event)=>CreateProduct()} >Créer ce produit</Button>)}
+            {product !== null ? (<Button variant="outlined" onClick={updateProduct} >Modifier ce produit</Button>): (<Button variant="outlined" onClick={(event)=>createProduct()} >Créer ce produit</Button>)}
       </Box>
     </div>
   )
